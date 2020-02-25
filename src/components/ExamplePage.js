425e553ab82fed93721a361dd4cc1c94020b0883
Haris Spahija <haris.spahija@pon.com>
2020-02-25 11:14:41 +0100

    Changed Example to work with Yoast Meta

--- src/components/ExamplePage.js
+++ src/components/ExamplePage.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Helmet } from "react-helmet";
 
 export default function ExamplePage(props) {
-    const { page } = props;
+    const {
+        page,
+        page: { yoast_title, yoast_meta }
+    } = props;
+
     return (
         <div class={"page"}>
+            <Helmet>
+                <title>{yoast_title}</title>
+                {yoast_meta.map(meta_value => {
+                    return (
+                        <meta
+                            name={meta_value.name || meta_value.property}
+                            content={meta_value.content}
+                        />
+                    );
+                })}
+            </Helmet>
             <h1 class={"page-title"}>{page.title.rendered}</h1>
-            <div class={"page-content"} dangerouslySetInnerHTML={{ __html: props.page.content.rendered }}/>
+            <div
+                class={"page-content"}
+                dangerouslySetInnerHTML={{
+                    __html: props.page.content.rendered
+                }}
+            />
         </div>
-    )
+    );
 }
