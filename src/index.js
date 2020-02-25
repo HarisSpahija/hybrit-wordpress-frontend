a97d59b0e526cbda370a0e059116620d867da785
Haris Spahija <haris.spahija@pon.com>
2020-02-25 11:14:47 +0100

    Added react snap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from "react-dom";
 import App from './App';
+import React from 'react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
